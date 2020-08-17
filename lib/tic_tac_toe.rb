9eaee2c62e9daddeb1af3b3b9d245457be78ba3a
Jessica Dior <jessicaclairdior@gmail.com>
2020-08-17 20:14:20 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -21,8 +21,8 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-def move(board, index)
-  board[index] = player
+def move(board, index, first_player = "X")
+  board[index] = first_player
 end
 
 
