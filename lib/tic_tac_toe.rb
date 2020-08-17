9408b416a4297211f3e230085215b04f7c99d86e
Jessica Dior <jessicaclairdior@gmail.com>
2020-08-17 20:22:40 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -47,7 +47,7 @@ def turn(board)
   user_input = gets.strip
   index = input_to_index(user_input)
   if valid_move?(board, index)
-      move(board, index, first_player = "X")
+      move(board, index, player)
       display_board(board)
   else
     turn(board)
